d59ab466a7521fac288c2ba6d1c1ad698c3a8259
Swasti Mishra <smishr11@vols.utk.edu>
2022-03-30 18:07:42 -0400

    Finished my_malloc
    It should work. I think... we'll see.

--- mymalloc.c
+++ mymalloc.c
@@ -1,8 +1,7 @@
 // Lab 7: mymalloc.c
 // The goal of this lab is to write my_malloc() and my_free(), 
 //      which should work just like malloc() and free()
-
-//      http://web.eecs.utk.edu/~jplank/plank/classes/cs360/360/labs/Lab-7-Malloc/index.html
+//      See http://web.eecs.utk.edu/~jplank/plank/classes/cs360/360/labs/Lab-7-Malloc/index.html
 //      for more information and lab specifications. 
 
 // COSC 360
@@ -10,21 +9,157 @@
 // Mar 28, 2022
 // James Plank 
 
+// Libraries
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
+// Our one global variable 
 void *head ;
 
+// Our node, modelled after Plank's
 struct Node{
 	size_t size ;
 	struct Node *blink ;
 	struct Node *flink ;
 } ;
 
-void *my_malloc(size_t size){} ;
-void my_free(void *ptr){} ;
+void *my_malloc( size_t size ){
+	
+    // Make sure the variable is divisible by 8 
+    if( ( size % 8 ) != 0 ){
+        size = ( ( size + 8 ) - ( size % 8 ) ) ;
+    }
+    size = ( size + 8 ) ;
+
+	// Start allocating memory
+    if(head == NULL){
+		long int ptr ;
+
+		// Allocates space in case size is larger than 8192
+		if( size > 8192 ){
+			ptr = size ;
+		}
+		else{ 
+			ptr = 8192 ;
+		}
+        head = sbrk( ptr ) ;
+        void * temp = head ;
+
+        // Check size
+		if( ptr >= ( size + 4 ) ){
+
+			// When the freelist is empty set chunk to head
+            head = ( head + size ) ;
+			struct Node *node = head ;
+			node->size = ( ptr - size ) ;
+			node->blink = NULL ;
+			node->flink = NULL ;
+        } 
+
+		// Otherwise, place at the top of the list 
+		else{
+			head = NULL ;
+		}
+
+		// Add padding 
+		int *i = temp ;
+		*i = size ;
+		return (void*)( (int)( temp + 8 ) ) ;
+	}
+
+	// Reset back to head 
+	struct	Node *node = head ;
+	int check = -1 ;
+	
+    // When the size doesn't need the whole chunk 
+    while(1){
+        if( node->size >= ( size + 4 ) ){
+			check = 0 ;
+			break ;
+		}
+		if( node->size >= ( size ) ){
+            check = 1 ;
+			break ;
+		}
+		if( node->flink == NULL ){
+            check = 2 ;
+			break ;
+		}
+		node = node->flink ;
+	}
+
+    // Make a new node if check returns zero 
+	if( check == 0 ){
+		
+        struct Node *new = (void*)( (int)node + size ) ;
+		
+        new->size  = node->size - size ;
+		new->blink = node->blink ;
+		new->flink = node->flink ;
+		
+		if( node->blink != NULL ){
+			node->blink->flink = new ;
+		}
+        if( node->flink != NULL ){
+			node->flink->blink = new ;
+		}
+		
+        node->size = size ;
+		
+        if( node == head ){
+			head = new ;
+		}
+
+		return (void*)( (int) node + 8 ) ;
+	}
+
+    // Delete a node 
+	if( check == 1 ){
+		if(node->blink != NULL){
+			node->blink->flink = node->flink ;
+		}
+        if(node->flink != NULL){
+			node->flink->blink = node->blink ;
+		}
+        node->size = size ;
+        if( head == node ){
+			if( node->flink != NULL ){
+				node->flink->blink = NULL ;
+			}
+            head = node->flink ;
+		}
+
+		return (void*)( (int) node + 8 ) ;
+	}
+
+	// If we need more memory- same procedure as above 
+    long int ptr ; 
+	if( size > 8192 ){
+		ptr = size ;
+	}
+	else{ 
+		ptr = 8192 ;
+	}
+	void *temp = sbrk( ptr ) ;
+
+    if( ptr >= ( size + 4 ) ){
+		struct Node *new = (void*)( ( (int)temp ) + size) ;
+		
+        new->size = ptr - size ;
+		new->blink = node ;
+		new->flink = NULL ;
+        node->flink = new ;
+	}
+	
+    int *i = temp ;
+	*i = size ;
+	
+    return (void*)( (int)temp + 8 ) ;
+}
+
+
+void my_free( void *ptr ){} ;
 void *free_list_begin(){} ;
-void *free_list_next(void *node){} ;
-int comparator(void * a, void * b){} ;
+void *free_list_next( void *node ){} ;
 void coalesce_free_list(){} ;
